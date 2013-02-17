2f3b3c4e5c8529ad8b51e1d93b172a24a8c0cb64
Roderik van der Veer <roderik@vanderveer.be>
2013-02-17 22:52:55 +0100

    add gitconfigs

---
+++ recipes/gitconfig.rb
@@ -0,0 +1,26 @@
+if node["platform"] == "ubuntu"
+    template "/etc/gitconfig" do
+        source "gitconfig.erb"
+        owner "root"
+        mode "0755"
+    end
+else
+    template "#{node['etc']['passwd'][node['current_user']]['dir']}/.gitconfig" do
+        source "gitconfig.erb"
+        owner "root"
+        mode "0755"
+    end
+end
+
+template "/etc/gitignore" do
+    source "gitignore.erb"
+    owner "root"
+    mode "0755"
+end
+
+if node["platform"] == "mac_os_x"
+    execute "set git system credential helper" do
+        command "git config --global credential.helper osxkeychain"
+        user node['current_user']
+    end
+end
