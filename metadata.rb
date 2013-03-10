7d228fd6c582791db4f4deb175e2be7f5c000a80
Roderik van der Veer <roderik@vanderveer.be>
2013-03-10 20:39:50 +0100

    Fix foodcritic

--- metadata.rb
+++ metadata.rb
@@ -6,4 +6,4 @@ description      'Installs/Configures dotfiles'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.0.1'
 
-depends "homebrew"
+depends "applications"
