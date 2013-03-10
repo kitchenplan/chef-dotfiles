ed4ec996882fc3b8d7a9a37128d2cb82bd291abc
Roderik van der Veer <roderik@vanderveer.be>
2013-03-10 21:15:11 +0100

    Fix linux

--- recipes/kdeploy.rb
+++ recipes/kdeploy.rb
@@ -1,11 +1,24 @@
 include_recipe 'applications::git'
 
+directory "/opt" do
+  owner "root"
+  group value_for_platform(
+                            "mac_os_x" => { "default" => "admin" },
+                            "default" => "root"
+                          )
+  mode 0744
+  action :create
+end
+
 # Getting the kdeploy sources
 git "/opt/kDeploy" do
   repository "git@github.com:Kunstmaan/kDeploy.git"
   reference "master"
   action :sync
-  user node['current_user']
+  user value_for_platform(
+                            "mac_os_x" => { "default" => node['current_user'] },
+                            "default" => "root"
+                          )
 end
 
 template "/opt/kDeploy/tools/config.xml" do
--- recipes/kms.rb
+++ recipes/kms.rb
@@ -1,13 +1,26 @@
 include_recipe 'applications::git'
 include_recipe 'dotfiles::bash_it'
 
+directory "/opt" do
+  owner "root"
+  group value_for_platform(
+                            "mac_os_x" => { "default" => "admin" },
+                            "default" => "root"
+                          )
+  mode 0744
+  action :create
+end
+
 # Getting the kms sources
 
 git "/opt/kms" do
   repository "https://github.com/Kunstmaan/kms.git"
   reference "master"
   action :checkout
-  user node['current_user']
+  user value_for_platform(
+                            "mac_os_x" => { "default" => node['current_user'] },
+                            "default" => "root"
+                          )
 end
 
 # Add to the bash_profile settings
