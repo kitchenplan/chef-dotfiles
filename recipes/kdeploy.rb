aff4fc65a9e7e3c798c014d1e6571982f4a71a20
stephaneb <stephane.bakelants@kunstmaan.be>
2013-03-19 10:34:27 +0100

    added the essentials for ubuntu and corrected the config.xml and added the creation of backupped-projects

--- recipes/kdeploy.rb
+++ recipes/kdeploy.rb
@@ -1,7 +1,11 @@
 include_recipe 'applications::git'
-include_recipe 'osxdefaults::finder_unhide_home'
+if platform?('mac_os_x')
+    include_recipe 'osxdefaults::finder_unhide_home'
+elsif platform_family?('debian')
+    include_recipe 'applications::essentials'
+end
 
-# Getting the kdeploy sources
+# Getting the kdeploy sources and place the correct values in the config.xml
 git "/opt/kDeploy" do
   repository "git@github.com:Kunstmaan/kDeploy.git"
   reference "master"
@@ -9,18 +13,42 @@ git "/opt/kDeploy" do
   user node["current_user"]
 end
 
+#set the correct parameters to use in the config.xml
+if Chef::Config[:solo]
+    hostname = node["dotfiles"]["kdeploy"]["hostname"] + ".kunstmaan.be"
+    if platform?('mac_os_x')
+        postgresuser = node['current_user']
+    elsif platform_family?('debian')
+        postgresuser = "postgres"
+    end
+else
+    hostname = node["dotfiles"]["kdeploy"]["hostname"] + ".kunstmaan.com"
+    postgresuser = "postgres"
+end
+
+#Set the config.xml file with the correct parameters
 template "/opt/kDeploy/tools/config.xml" do
   source "kdeploy.conf.erb"
   owner "root"
   mode "0755"
   variables(
-    :user => node['current_user'],
+    :user => postgresuser,
     :password => node["mysql_root_password"],
-    :hostname => node["dotfiles"]["kdeploy"]["hostname"]
+    :hostname => hostname
   )
 end
 
+#Create the project directory
 directory "/home/projects" do
+  owner "root"
+  group "admin"
+  mode 0777
+  action :create
+  recursive true
+end
+
+#Create the backupped-projects directory
+directory "/home/backupped-projects" do
   owner "root"
   group "admin"
   mode 0777
