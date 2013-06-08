00c3c25c10b37724326428fdfa944ed86d9d2d7f
Roderik van der Veer <roderik@vanderveer.be>
2013-06-08 20:39:24 +0200

    Permissions for the bashit files

--- providers/bash_it_custom_plugin.rb
+++ providers/bash_it_custom_plugin.rb
@@ -5,6 +5,7 @@ action :create do
   template ::File.expand_path(script_name, bash_it_custom_dir) do
     source new_resource.name
     owner node['current_user']
+    mode "0755"
     only_if { script_name =~ /\.bash$/ && ::File.directory?(bash_it_custom_dir) }
     cookbook new_resource.cookbook
   end
