d5352d26e09aa2957c4b411e633a634830ce68e0
Thomas Meeus <thomas.meeus@kunstmaan.be>
2013-06-13 17:34:40 +0200

    Add ssh-config

--- recipes/kdeploy.rb
+++ recipes/kdeploy.rb
@@ -134,3 +134,20 @@ script "configure logrotate" do
         chmod u+s logrotate
     EOH
 end
+
+
+
+## SSH-config
+if platform_family?('debian')
+    template "/etc/ssh/ssh_config" do
+        source "ssh_config.erb"
+        owner "root"
+        mode "0644"
+    end
+else
+    template "/etc/ssh_config" do
+        source "ssh_config.erb"
+        owner "root"
+        mode "0644"
+    end
+end
