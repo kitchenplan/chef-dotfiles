c239783947f9c40617b9fe85ccb5bba47a3ae49a
Roderik van der Veer <roderik@vanderveer.be>
2013-07-23 17:27:54 +0200

    Install the vim settings globally

--- recipes/vim.rb
+++ recipes/vim.rb
@@ -3,14 +3,17 @@
 #    action [:install, :upgrade]
 #end
 
-directory "#{node['etc']['passwd'][node['current_user']]['dir']}/.vim/autoload" do
+vimrc_path = node["platform"] == "ubuntu" ? "/etc/vim/vimrc" : "#{node['etc']['passwd'][node['current_user']]['dir']}/.vimrc"
+vimplugin_prefix = node["platform"] == "ubuntu" ? "/etc/vimfiles" : "#{node['etc']['passwd'][node['current_user']]['dir']}/.vim"
+
+directory "#{vimplugin_prefix}/autoload" do
   owner node['current_user']
   mode 00755
   recursive true
   action :create
 end
 
-remote_file "#{node['etc']['passwd'][node['current_user']]['dir']}/.vim/autoload/pathogen.vim" do
+remote_file "#{vimplugin_prefix}/autoload/pathogen.vim" do
   source "https://raw.github.com/tpope/vim-pathogen/master/autoload/pathogen.vim"
   mode 00755
   action :create_if_missing
@@ -18,7 +21,7 @@ end
 
 node['dotfiles']['vim'].each do |folder, repohash|
 
-    directory "#{node['etc']['passwd'][node['current_user']]['dir']}/.vim/#{folder}" do
+    directory "#{vimplugin_prefix}/#{folder}" do
         owner node['current_user']
 	mode 00755
 	recursive true
@@ -26,7 +29,7 @@ node['dotfiles']['vim'].each do |folder, repohash|
 
     repohash.each do |repos|
         repos.each do |repo|
-            git "#{node['etc']['passwd'][node['current_user']]['dir']}/.vim/#{folder}/#{repo[0]}" do
+            git "#{vimplugin_prefix}/#{folder}/#{repo[0]}" do
                 repository repo[1]
                 enable_submodules true
                 action :sync
@@ -37,19 +40,19 @@ node['dotfiles']['vim'].each do |folder, repohash|
 
 end
 
-directory "#{node['etc']['passwd'][node['current_user']]['dir']}/.vim/colors" do
+directory "#{vimplugin_prefix}/colors" do
   owner node['current_user']
   mode 00755
   recursive true
   action :create
 end
 
-remote_file "#{node['etc']['passwd'][node['current_user']]['dir']}/.vim/colors/Tomorrow-Night-Eighties.vim" do
+remote_file "#{vimplugin_prefix}/colors/Tomorrow-Night-Eighties.vim" do
     source "https://raw.github.com/chriskempson/tomorrow-theme/master/vim/colors/Tomorrow-Night-Eighties.vim"
     mode 00755
     action :create_if_missing
 end
 
-template "#{node['etc']['passwd'][node['current_user']]['dir']}/.vimrc" do
+template vimrc_path do
   source "vimrc.erb"
 end
