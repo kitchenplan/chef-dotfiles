15c0c22602289acdc2ec170cecb36e6b67fbb6c1
Roderik van der Veer <roderik@vanderveer.be>
2013-10-04 23:23:42 +0200

    Normal history behaviour

--- templates/default/bash_it/custom/exports.bash
+++ templates/default/bash_it/custom/exports.bash
@@ -18,7 +18,7 @@ export HISTFILESIZE=100000               # big big history
 shopt -s histappend                      # append to history, don't overwrite it
 
 # Save and reload the history after each command finishes
-export PROMPT_COMMAND="history -a; history -c; history -r; echo -ne \"\033]0;${USER}@${HOSTNAME}\007\"; $PROMPT_COMMAND"
+#export PROMPT_COMMAND="history -a; history -c; history -r; echo -ne \"\033]0;${USER}@${HOSTNAME}\007\"; $PROMPT_COMMAND"
 
 #export PGHOST=/var/run/postgresql/
 
@@ -31,6 +31,6 @@ export NODE_PATH=/usr/local/lib/node_modules:$NODE_PATH
 systemsetup -gettimezone | sed 's/Time\ Zone:\ //g' | xargs sh -c "export TZ='$1'"
 
 # fix multibyte errors in ruby 1.9.x
-export RUBYOPT='-Ku' 
+export RUBYOPT='-Ku'
 
 export HOMEBREW_CASK_OPTS="--appdir=/Applications"
