5704fcb84bbb4e92e505bda907c1e25bd15f76ce
Roderik van der Veer <roderik.van.der.veer@kunstmaan.be>
2014-01-18 19:39:53 +0100

    Update gem_setup.rb

--- recipes/gem_setup.rb
+++ recipes/gem_setup.rb
@@ -1,8 +1,13 @@
 dotfiles_bash_it_custom_plugin "gem_setup-warn_on_sudo.bash"
 
+thegroup = value_for_platform(
+    ["mac_os_x"] => { "default" => "wheel"},
+    "default" => "root"
+)
+
 file "/etc/gemrc" do
   owner "root"
-  group "wheel"
+  group thegroup
   mode "0644"
   action :create
   content "install: --no-rdoc --no-ri\nupdate: --no-rdoc --no-ri\n"
