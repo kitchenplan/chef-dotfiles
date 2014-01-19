43c9246633308046ae9354b8d6668829ad2847e8
Roderik van der Veer <roderik.van.der.veer@kunstmaan.be>
2014-01-19 10:26:08 +0100

    Update vim.rb

--- recipes/vim.rb
+++ recipes/vim.rb
@@ -1,14 +1,14 @@
 node['dotfiles']['vimusers'].each do |user|
   homepath = lambda { node['etc']['passwd'][user]['dir'] }
 
-  directory "#{homepath}/.vim/autoload" do
+  directory "#{homepath.call}/.vim/autoload" do
     owner user
     mode 00755
     recursive true
     action :create
   end
 
-  remote_file "#{homepath}/.vim/autoload/pathogen.vim" do
+  remote_file "#{homepath.call}/.vim/autoload/pathogen.vim" do
     source "https://raw.github.com/tpope/vim-pathogen/master/autoload/pathogen.vim"
     mode 00755
     owner user
@@ -16,14 +16,14 @@ node['dotfiles']['vimusers'].each do |user|
   end
 
   node['dotfiles']['vim'].each do |folder, repohash|
-    directory "#{homepath}/.vim/#{folder}" do
+    directory "#{homepath.call}/.vim/#{folder}" do
       owner user
       mode 00755
       recursive true
     end
     repohash.each do |repos|
       repos.each do |repo|
-        git "#{homepath}/.vim/#{folder}/#{repo[0]}" do
+        git "#{homepath.call}/.vim/#{folder}/#{repo[0]}" do
           repository repo[1]
           enable_submodules true
           action :sync
@@ -33,21 +33,21 @@ node['dotfiles']['vimusers'].each do |user|
     end
   end
 
-  directory "#{homepath}/.vim/colors" do
+  directory "#{homepath.call}/.vim/colors" do
     owner user
     mode 00755
     recursive true
     action :create
   end
 
-  remote_file "#{homepath}/.vim/colors/Tomorrow-Night-Eighties.vim" do
+  remote_file "#{homepath.call}/.vim/colors/Tomorrow-Night-Eighties.vim" do
       source "https://raw.github.com/chriskempson/tomorrow-theme/master/vim/colors/Tomorrow-Night-Eighties.vim"
       mode 00755
       owner user
       action :create_if_missing
   end
 
-  template "#{homepath}/.vimrc" do
+  template "#{homepath.call}/.vimrc" do
     source "vimrc.erb"
     owner user
   end
