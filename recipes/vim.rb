a687325c98df3eef86b521cc55b7f7bbc7cb6f6a
Roderik van der Veer <roderik.van.der.veer@kunstmaan.be>
2014-01-19 09:37:56 +0100

    Update vim.rb

--- recipes/vim.rb
+++ recipes/vim.rb
@@ -1,15 +1,14 @@
 node['dotfiles']['vimusers'].each do |user|
-  vimrc_path = "#{node['etc']['passwd'][user]['dir']}/.vimrc"
-  vimplugin_prefix = "#{node['etc']['passwd'][user]['dir']}/.vim"
+  homepath = lambda { node['etc']['passwd'][user]['dir'] }
 
-  directory "#{vimplugin_prefix}/autoload" do
+  directory "#{homepath}/.vim/autoload" do
     owner user
     mode 00755
     recursive true
     action :create
   end
 
-  remote_file "#{vimplugin_prefix}/autoload/pathogen.vim" do
+  remote_file "#{homepath}/.vim/autoload/pathogen.vim" do
     source "https://raw.github.com/tpope/vim-pathogen/master/autoload/pathogen.vim"
     mode 00755
     owner user
@@ -17,14 +16,14 @@ node['dotfiles']['vimusers'].each do |user|
   end
 
   node['dotfiles']['vim'].each do |folder, repohash|
-    directory "#{vimplugin_prefix}/#{folder}" do
+    directory "#{homepath}/.vim/#{folder}" do
       owner user
       mode 00755
       recursive true
     end
     repohash.each do |repos|
       repos.each do |repo|
-        git "#{vimplugin_prefix}/#{folder}/#{repo[0]}" do
+        git "#{homepath}/.vim/#{folder}/#{repo[0]}" do
           repository repo[1]
           enable_submodules true
           action :sync
@@ -34,21 +33,21 @@ node['dotfiles']['vimusers'].each do |user|
     end
   end
 
-  directory "#{vimplugin_prefix}/colors" do
+  directory "#{homepath}/.vim/colors" do
     owner user
     mode 00755
     recursive true
     action :create
   end
 
-  remote_file "#{vimplugin_prefix}/colors/Tomorrow-Night-Eighties.vim" do
+  remote_file "#{homepath}/.vim/colors/Tomorrow-Night-Eighties.vim" do
       source "https://raw.github.com/chriskempson/tomorrow-theme/master/vim/colors/Tomorrow-Night-Eighties.vim"
       mode 00755
       owner user
       action :create_if_missing
   end
 
-  template vimrc_path do
+  template "#{homepath}/.vimrc" do
     source "vimrc.erb"
     owner user
   end
