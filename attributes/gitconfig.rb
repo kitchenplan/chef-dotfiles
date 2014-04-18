e110f6ddc1acee45f201232ca128f7795cb09bb6
Roderik van der Veer <roderik.van.der.veer@kunstmaan.be>
2014-04-18 21:02:21 +0200

    Add some aliasses

--- attributes/gitconfig.rb
+++ attributes/gitconfig.rb
@@ -3,16 +3,15 @@ node.default["git"]["email"] = "you@example.com"
 
 # set of helpful aliases
 node.default["git"]["aliases"] = {}.tap do |aliases|
-    aliases['l'] = 'log --pretty=oneline -n 20 --graph'
-    aliases['s'] = 'status -s'
-    aliases['d'] = 'diff --patch-with-stat'
-    aliases['p'] = '!"git pull; git submodule foreach git pull origin master"'
-    aliases['c'] = 'clone --recursive'
-    aliases['ca'] = '!git add -A && git commit -av'
-    aliases['go'] = 'checkout -B'
-    aliases['tags'] = 'tag -l'
-    aliases['remotes'] = 'remote -v'
-    aliases['credit'] = '"!f() { git commit --amend --author \"$1 <$2>\" -C HEAD; }; f"'
-    aliases['undopush'] = 'push -f origin HEAD^:master'
-    aliases['pom'] = 'push origin master'
+    aliases['l'] = "log --all --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --date=relative"
+    aliases['d'] = "diff --patch-with-stat"
+    aliases['c'] = "clone --recursive"
+    aliases['ca'] = "!git add -A && git commit -av"
+    aliases['go'] = "checkout -B"
+	aliases['tags'] = "tag -l"
+	aliases['remotes'] = "remote -v"
+    aliases['undopush'] = "push -f origin HEAD^:master"
+    aliases['pom'] = "push origin master"
+	aliases['s'] = "status -sb"
+	aliases['branches'] = "branch -a"
 end
