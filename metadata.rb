1c9409b25e316b1ecc7e9f4affd10953c0003b6c
Roderik van der Veer <roderik.van.der.veer@kunstmaan.be>
2014-10-07 07:46:34 +0200

    Update metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -3,6 +3,6 @@ maintainer       'Roderik van der Veer'
 maintainer_email 'roderik@vanderveer.be'
 license          'Apache 2.0'
 description      'Installs/Configures dotfiles'
-version          '0.0.1'
+version          '1.3.2'
 
 depends "osxdefaults"
